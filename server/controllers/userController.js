016b563389d80acb507abd63a16ce11952c89fa4
Abhishek madoliya <abhishekmadoliya@gmail.com>
2025-04-17 12:54:53 +0530

    express started

---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+
+// const userController = async (req,res)=>{
+//     try {
+//         res.send("hellow world")
+//     } catch (err) {
+//         console.log("error in user controller", err);
+        
+//     }
+// }
+
+
+// module.exports = userController;
--- server/index.js
+++ server/index.js
@@ -1,25 +1,30 @@
-const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
+const express = require("express");
 const app = express();
-const listingRoutes = require('./routes/listingroutes');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
+const listingRoutes = require("./routes/listingroutes");
+const cors = require("cors");
+const mongoose = require("mongoose");
+// const { default: userController } = require('./controllers/userController');
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors())
-// app.get('/',(req,res)=>{
-//     res.send("hello world");
-// })
+app.use(cors());
+
 async function Db_connect() {
-    await mongoose.connect('mongodb://localhost:27017/ecommerce').then(()=>console.log("db connected")).catch((e)=>console.log("error in db connection", e));
-   
+    await mongoose
+    .connect("mongodb://localhost:27017/ecommerce")
+    .then(() => console.log("db connected"))
+    .catch((e) => console.log("error in db connection", e));
 }
 
 Db_connect();
 
-app.use('/listings', listingRoutes);
-
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+// used in client : ecommerce
+app.use("/listings", listingRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
